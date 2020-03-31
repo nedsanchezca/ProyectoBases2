f8df771298d5b1892474a724445a137f642c0687
Juan Avila <ag.juandiego@gmail.com>
2020-03-31 01:54:36 -0500

    Modificaciones para la clasificación de representantes

--- ClasificacionRepresentante.sql
+++ ClasificacionRepresentante.sql
@@ -1,10 +1,19 @@
 SET ServerOutput ON
 SET Verify OFF
+
+CREATE SEQUENCE SEQ_HISTO
+    INCREMENT BY 1
+    START WITH 1
+    NOCACHE
+    NOCYCLE;
+
 -- Totalizar Carrito con IVA
 CREATE OR REPLACE PROCEDURE PR_clasificarRepresentante(pd_fecha_inicial historico_clasificacion.d_fecha_inicial%TYPE)
 AS
 -- Declaración de variables
     l_aux NUMBER := 0;
+    l_clasificacion clasificacion.K_ID%TYPE;
+    l_calificacionAsignada clasificacion.K_ID%TYPE;
 -- Declaración de Cursores
     -- Cursor para consultar el total de ventas de cada representante
     CURSOR c_totalVentas IS
@@ -44,9 +53,15 @@ AS
 
     -- Cursor para redefinir la nueva clasificación de cada representante
     CURSOR c_nuevaClasificacion IS
-        SELECT
-        FROM
-        WHERE
+        SELECT CL.N_NOMBRE clasificacion, F_ID_REP_VENTAS, PE.N_NOMBRE nom, N_APELLIDO, V_VENTA_MINIMA, CL.K_ID, HC.F_TIPO_ID
+        FROM CLASIFICACION CL, PERSONA PE, REP_VENTAS RV, PEDIDO P, HISTORICO_CLASIFICACION HC, CLIENTE C
+        WHERE   PE.K_NUMERO_ID = RV.F_NUMERO_ID AND
+                C.F_ID_REP_VENTAS = RV.F_NUMERO_ID AND
+                PE.K_TIPO_ID = RV.F_TIPO_ID AND
+                CL.K_ID = HC.F_ID_CLASIFICACION AND
+                RV.F_NUMERO_ID = HC.F_NUM_ID AND
+                d_fecha >= TO_DATE('12.08.2019', 'DD.MM.YYYY') AND d_fecha <= SYSDATE
+        GROUP BY F_ID_REP_VENTAS, CL.N_NOMBRE, PE.N_NOMBRE, N_APELLIDO, V_VENTA_MINIMA, CL.K_ID, HC.F_TIPO_ID;
     lc_nuevaClasificacion c_nuevaClasificacion%ROWTYPE;
 
 BEGIN
@@ -84,6 +99,30 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE(lc_promedio.valoracion/lc_promedio.cantidadClientes||' - '||lc_promedio.N_NOMBRE);
     END LOOP;
     CLOSE c_promedioCalificacion;
+
+    -- Cursor para redefinir la nueva clasificación de cada representante
+    OPEN c_nuevaClasificacion;
+    DBMS_OUTPUT.PUT_LINE('Categoria anterior | Categoria asignada | Nombre Representante');
+    DBMS_OUTPUT.PUT_LINE('-----------------------------------');
+    LOOP
+        FETCH c_nuevaClasificacion INTO lc_nuevaClasificacion;
+        EXIT WHEN c_nuevaClasificacion%NOTFOUND;
+        l_clasificacion := lc_nuevaClasificacion.K_ID;
+        IF lc_nuevaClasificacion.V_VENTA_MINIMA < 4000 THEN -- 1.000.000
+            l_calificacionAsignada := lc_nuevaClasificacion.K_ID - 1;
+            INSERT INTO HISTORICO_CLASIFICACION VALUES(SEQ_HISTO.NEXTVAL,TO_DATE('12.08.2019', 'DD.MM.YYYY'),SYSDATE,lc_nuevaClasificacion.F_ID_REP_VENTAS,lc_nuevaClasificacion.F_TIPO_ID,l_calificacionAsignada);
+        ELSIF lc_nuevaClasificacion.V_VENTA_MINIMA < 3000 THEN -- 500.000
+            l_calificacionAsignada := lc_nuevaClasificacion.K_ID - 1;
+            INSERT INTO HISTORICO_CLASIFICACION VALUES(SEQ_HISTO.NEXTVAL,TO_DATE('12.08.2019', 'DD.MM.YYYY'),SYSDATE,lc_nuevaClasificacion.F_ID_REP_VENTAS,lc_nuevaClasificacion.F_TIPO_ID,l_calificacionAsignada);
+        ELSIF lc_nuevaClasificacion.V_VENTA_MINIMA < 2000 THEN -- 200.000
+            l_calificacionAsignada := lc_nuevaClasificacion.K_ID - 1;
+            INSERT INTO HISTORICO_CLASIFICACION VALUES(SEQ_HISTO.NEXTVAL,TO_DATE('12.08.2019', 'DD.MM.YYYY'),SYSDATE,lc_nuevaClasificacion.F_ID_REP_VENTAS,lc_nuevaClasificacion.F_TIPO_ID,l_calificacionAsignada);
+        ELSE
+            l_calificacionAsignada := lc_nuevaClasificacion.K_ID;
+        END IF;
+        DBMS_OUTPUT.PUT_LINE(l_clasificacion||' '||l_calificacionAsignada||' '||lc_nuevaClasificacion.nom||' '||lc_nuevaClasificacion.N_APELLIDO);
+    END LOOP;
+    CLOSE c_nuevaClasificacion;
 END PR_clasificarRepresentante;
 /
 
