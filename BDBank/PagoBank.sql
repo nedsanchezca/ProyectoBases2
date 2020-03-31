4acef1dc97245c030de9abfdcd15ff992f26a416
Juan Avila <ag.juandiego@gmail.com>
2020-03-30 21:46:47 -0500

    Inserción de datos en la simulación del banco a la tabla PAGO

--- BDBank/PagoBank.sql
+++ BDBank/PagoBank.sql
@@ -1,6 +1,11 @@
 SET ServerOutput ON
 SET Verify OFF
 -- Realizar pago del banco
+CREATE SEQUENCE SEQ_PAGO
+    INCREMENT BY 1
+    START WITH 1
+    NOCACHE
+    NOCYCLE;
 CREATE OR REPLACE PROCEDURE PR_pagoBanco(pfk_cuentaorigen pago.fk_cuentaorigen%TYPE,
                                         pfk_cuentadestino pago.fk_cuentadestino%TYPE,
                                         pn_montopago pago.n_montopago%TYPE)
@@ -19,6 +24,7 @@ BEGIN
         FETCH c_pagoBanco INTO lc_pagoBanco;
         EXIT WHEN c_pagoBanco%NOTFOUND;
         DBMS_OUTPUT.PUT_LINE('Correcto');
+        INSERT INTO PAGO VALUES(SEQ_PAGO.NEXTVAL,pn_montopago,pfk_cuentaorigen,pfk_cuentadestino);
     END LOOP;
     CLOSE c_pagoBanco;
 
