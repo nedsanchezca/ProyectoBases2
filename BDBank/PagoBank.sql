b345acde0db6aae29afc8e71b9217de41ae81459
Juan Avila <ag.juandiego@gmail.com>
2020-03-30 21:26:43 -0500

    Modificación del procedimiento para realizar el pago en el banco

--- BDBank/PagoBank.sql
+++ BDBank/PagoBank.sql
@@ -6,7 +6,27 @@ CREATE OR REPLACE PROCEDURE PR_pagoBanco(pfk_cuentaorigen pago.fk_cuentaorigen%T
                                         pn_montopago pago.n_montopago%TYPE)
 AS
 -- Declaración de variables
+    l_noDatos EXCEPTION;
 -- Declaración de cursores
+    CURSOR c_pagoBanco IS
+        SELECT K_CUENTA
+        FROM CUENTA C, CLIENTE CE
+        WHERE C.FK_CLIENTE = CE.K_CLIENTE;
+    lc_pagoBanco c_pagoBanco%ROWTYPE;
 BEGIN
-    INSERT 
+    OPEN c_pagoBanco;
+    LOOP
+        FETCH c_pagoBanco INTO lc_pagoBanco;
+        EXIT WHEN c_pagoBanco%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('Correcto');
+    END LOOP;
+    CLOSE c_pagoBanco;
+
+    IF lc_pagoBanco.K_CUENTA <> pfk_cuentaorigen THEN
+        RAISE l_noDatos;
+    END IF;
+EXCEPTION
+    WHEN l_noDatos THEN
+        RAISE_APPLICATION_ERROR(-20113,'No se encontró una cuenta asociada al banco');
 END PR_pagoBanco;
+/
